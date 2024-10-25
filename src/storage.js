9aaeb49c0bb44a05796e9e12f9c05cfcf3cdd333
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-10-25 21:09:29 +0900

    データ共有機能実装

---
+++ src/storage.js
@@ -0,0 +1,52 @@
+import { ref } from "vue"
+
+const storageName = "storageOfTranscriptionApp"
+const allData = ref()
+
+// 初期化
+if (window.localStorage.getItem(storageName) == null) {
+    window.localStorage.setItem(storageName, "[]")
+    allData.value = []
+} else {
+    allData.value = JSON.parse(window.localStorage.getItem(storageName))
+}
+
+export function getAllData() {
+    return allData
+}
+
+export function createData(data) {
+    data.id = generateUUID()
+    data.date = new Date().toLocaleString()
+    allData.value.push(data)
+    saveStorageData(allData)
+}
+
+export function updateData(data) {
+    for (let i of allData.value) {
+        console.log(i)
+        if (i.id == data.id) {
+            i.title = data.title
+            i.document = data.document
+            i.date = new Date().toLocaleString()
+        }
+    }
+    saveStorageData(allData)
+}
+
+export function deleteData(id) {
+    allData.value = allData.value.filter(item => item.id != id)
+    saveStorageData(allData)
+}
+
+function saveStorageData(localData) {
+    window.localStorage.setItem(storageName, JSON.stringify(localData.value))
+}
+
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+          v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
