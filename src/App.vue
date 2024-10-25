3359e74808c64f50d8c1c1358b66e3ad3a799736
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-10-25 21:09:59 +0900

    メインページ開発

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-</template>
-
 <script setup>
+import { provide } from 'vue'
+import { getAllData } from './storage'
+
+const allData = getAllData()
+provide("allData", allData)
 </script>
+
+<template>
+    <v-app>
+        <v-main>
+                <router-view/>
+        </v-main>
+    </v-app>
+</template>
