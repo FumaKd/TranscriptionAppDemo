aff6e81e4c0986611750269e6a7e0002dff50e73
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-10-26 16:28:13 +0900

    pathの修正

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "docs",
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue_practice_bus_guide/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/transcription_app_demo/' : '/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
