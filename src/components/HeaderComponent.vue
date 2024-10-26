473c326b36c673cbb7550c41bb027a2c5bb0385c
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-10-26 16:16:07 +0900

    prepare for githun pages

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,17 +4,9 @@
 <template>
     <v-app-bar
         color="#BC671D"
-        dark
     >
         <v-app-bar-nav-icon class="text-grey-lighten-2"></v-app-bar-nav-icon>
-        <v-toolbar-title class="text-grey-lighten-2">議事録アプリ</v-toolbar-title>
-        <!-- <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-  
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn> -->
+        <v-toolbar-title class="text-grey-lighten-2" @click="$router.push({name: 'home'})">議事録アプリ</v-toolbar-title>
     </v-app-bar>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  outputDir: "docs",
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_practice_bus_guide/' : '/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
